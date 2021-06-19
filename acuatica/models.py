9b61a8ed5ac745be89d126548bc977867db43cb6
Donovan Vargas <dvargas@birkman.com>
2021-06-18 20:20:28 -0500

    inputs table

--- acuatica/models.py
+++ acuatica/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from multiselectfield import MultiSelectField
+
 
 class UserExtends(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_extend', verbose_name='usuario')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_extend', verbose_name='usuario')    
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)    
     is_admin = models.BooleanField(default=False)
@@ -48,6 +50,7 @@ class Clients(models.Model):
     )
 
     name = models.CharField(max_length=50)
+    picture = models.ImageField(verbose_name='foto', null=True, blank=True)
     last_name = models.CharField(max_length=50)
     second_last_name = models.CharField(max_length=50)
     birth_date = models.DateField(blank=True)
@@ -70,6 +73,70 @@ class Clients(models.Model):
 
     class Meta(object):
         verbose_name_plural = 'Clientes'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Inventario(models.Model):
+    BIG = 'B'
+    MEDIUM = 'M'
+    SMALL = 'S'
+    SIZE_CHOICE = (
+        (BIG, 'Grande'),
+        (MEDIUM, 'Mediano'),
+        (SMALL, 'Chico'),
+
+    )
+
+    ACUATICA = 'A'
+    AJUSTES_CONTABLES = 'AJ'
+    MANTENIMIENTO = 'M'
+    TIENDITA = 'T'
+    BRANCH_CHOICES = (
+        (ACUATICA, 'Acuatica Urioste'),
+        (AJUSTES_CONTABLES, 'Ajustes Contables'),
+        (MANTENIMIENTO, 'Mantenimineto'),
+        (TIENDITA, 'Tiendita'),
+    )
+
+    numcode = models.CharField(max_length=100)
+    # categoria A
+    # categoria B
+    name = models.TextField()
+    visible = models.BooleanField()
+    size = models.CharField(max_length=20, choices=SIZE_CHOICE)
+    measures = models.CharField(max_length=200)
+    # descripcion cuadro bien complejo, creo que seria mejor hacerlo en el front end
+    short_name = models.CharField(max_length=60)
+    branches = MultiSelectField(choices=BRANCH_CHOICES, blank=True, null=True)
+    limited_inventory = models.BooleanField()
+    contained_products = models.CharField(max_length=50)
+    minimum_amount = models.CharField(max_length=200)
+    max_amount = models.CharField(max_length=100)
+    colors = models.CharField(max_length=100)
+    browserdescription = models.CharField(max_length=100)
+    browserTitle = models.CharField(max_length=50)
+    friendly_url = models.CharField(max_length=100)
+
+    class Meta(object):
+        verbose_name_plural = 'Inventario'
+
+    def __str__(self):
+        return str(self.name)
+
+class Inputs(models.Model):
+    inventory=models.ForeignKey(Inventario, on_delete=models.DO_NOTHING)
+    cuantity = models.IntegerField()
+    date = models.DateField(blank=True)
+    sale = models.IntegerField()
+    comments = models.TextField()
+
+    cost = models.IntegerField()
+
+
+    class Meta(object):
+        verbose_name_plural = 'Entradas y Salidas'
 
     def __str__(self):
         return str(self.name)

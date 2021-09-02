0ce4045bdbcf2797ba20c2f78e15b109744db8b2
Donovan <dvargas@birkman.com>
2021-09-02 11:51:49 -0500

    arreglar modals

--- acuatica/static/scripts/base.js
+++ acuatica/static/scripts/base.js
@@ -5,3 +5,12 @@ function onclickhandler(e) {
   document.getElementById("coins").textContent = "$" + coins;
 }
 
+function get_photo(){
+
+  updatefotoclient = document.getElementById("id_image_source").value;
+  console.log(updatefotoclient);
+      //updatefotoclient.style.display = "none"
+
+  document.getElementById("display").src = updatefotoclient;
+  texto_oculto = document.getElementById("id_image_source")  
+};
